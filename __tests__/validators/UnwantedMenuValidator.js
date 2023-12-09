9adba137020bdd0d0eebab57d576d9ccc9fc3ab8
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-12-09 19:49:18 +0900

    test: 못 먹는 메뉴 유효성 검사 테스트 추가

--- __tests__/validators/UnwantedMenuValidator.js
+++ __tests__/validators/UnwantedMenuValidator.js
@@ -1,18 +1,25 @@
-import CONSTANTS from '../../src/constants/constants.js';
 import ERROR from '../../src/constants/error.js';
+import UnwantedMenuValidator from '../../src/utils/validators/UnwantedMenuValidator.js';
 
-class UnwantedMenuValidator {
-  static validateUnwantedMenu(menus) {
-    const validators = [this.#validateSeparator];
-    validators.forEach(validator => validator(menus));
-  }
+describe('못 먹는 메뉴 예외 상황 테스트', () => {
+  const cases = [
+    {
+      input: ',,adf,,',
+      description: '구분을 잘못하였을 경우 예외처리를 한다.',
+      expected: ERROR.unwantedMenu.separator,
+    },
+    {
+      input: 'asd, wer, df',
+      description: '최소 0개, 최대 2개의 못 먹는 메뉴가 아닌 경우 예외처리를 한다.',
+      expected: ERROR.unwantedMenu.count,
+    },
+  ];
 
-  static #validateSeparator(menus) {
-    const formattedMenus = menus.split(CONSTANTS.unwantedMenu.separator).map(name => name.trim());
-    const emptyCount = formattedMenus.filter(menu => !menu.trim()).length;
+  test.each(cases)('코치 이름 $input을 통해 에러를 반환한다.', ({ input, expected }) => {
+    // when
+    const result = () => UnwantedMenuValidator.validateUnwantedMenu(input);
 
-    if (emptyCount > CONSTANTS.number.zero) throw new Error(ERROR.unwantedMenu.separator);
-  }
-}
-
-export default UnwantedMenuValidator;
+    // then
+    expect(result).toThrow(expected);
+  });
+});
