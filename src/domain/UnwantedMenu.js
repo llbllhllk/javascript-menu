5657e36869e83d91c6c4ff2fd4c540292bf0c79a
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-12-09 20:10:42 +0900

    feat: UnwantedMenu domain 추가

---
+++ src/domain/UnwantedMenu.js
@@ -0,0 +1,23 @@
+import UnwantedMenuValidator from '../utils/validators/UnwantedMenuValidator.js';
+
+class UnwantedMenu {
+  #unwantedMenu;
+
+  constructor(menus) {
+    UnwantedMenuValidator.validateUnwantedMenu(menus);
+    this.#unwantedMenu = this.#formatUnwantedMenu(menus);
+  }
+
+  getUnwantedMenu() {
+    return this.#unwantedMenu;
+  }
+
+  #formatUnwantedMenu(menus) {
+    return menus
+      .split(CONSTANTS.coachNames.separator)
+      .map(name => name.trim())
+      .filter(Boolean);
+  }
+}
+
+export default UnwantedMenu;
